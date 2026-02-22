8f22edb10e536f8af75eda06ed32a36067131fe0
anibal yucra <anibalyucrac@gmail.com>
2026-02-21 20:40:49 -0500

    Update IntegracionController.php

--- app/controllers/Api/IntegracionController.php
+++ app/controllers/Api/IntegracionController.php
@@ -572,11 +572,11 @@ class IntegracionController extends BaseApiController
             $data = json_decode($json_data, true);
             $id_section = $data['id_section'];
             $courseid = $data['courseid'];
-            $this->json($data);
-            /*$this->json([
+            //$this->json($data);
+            $this->json([
                 'success' => true,
                 'message' => $this->serviceMoodle->getSectionData($id_section, $courseid, $MOODLE_URL, $MOODLE_TOKEN)
-            ]);*/
+            ]);
         } else {
             $this->json([
                 'success' => false,
